55d1d029743cfce65c6400ce585f6158fafd7f89
Prtm2110 <prathamhole@gmail.com>
2024-12-28 14:58:27 +0530

    Added Window Screen Logic

--- Spraky-core/main.cpp
+++ Spraky-core/main.cpp
@@ -1,11 +1,24 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include  "x64/src/graphics/window.h"
+
 
 int main() {
-	if (!glfwInit())
-		std::cout<<"Error" << std::endl;
-	else
-		std::cout<<"Sucess" << std::endl;
-	system("PAUSE");
+	using namespace sparky;
+	using namespace graphics;
+	Window window("sparky!!", 800, 600);
+	glClearColor(0.2f, 0.3f, 0.8f, 0.9f);
+
+	while (!window.closed())
+	{
+		window.clear();
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f,-0.5f);
+		glVertex2f(0.5f,0.5f);
+		glVertex2f(0.5f,-0.5f);
+		glEnd();
+
+		window.update();
+	}
 	return 0;
 }
