41c6deee37be4f37cced01e3b924f4818625d025
Prtm2110 <prathamhole@gmail.com>
2024-12-28 21:01:40 +0530

    Acutal Window Logic

---
+++ Spraky-core/x64/src/graphics/window.cpp
@@ -0,0 +1,61 @@
+#include "window.h"
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+namespace sparky {
+	namespace graphics {
+
+		void windowResize(GLFWwindow* window, int width, int height);
+		Window::Window(const char* title, int width, int height) {
+			m_Title = title;
+			m_Width = width;
+			m_Height = height;
+			m_Closed = false;
+			if (!init())
+				glfwTerminate();
+		}
+
+		Window::~Window() {
+			glfwDestroyWindow(m_Window);
+			glfwTerminate();
+		}
+
+		bool Window::init() 
+		{
+			if (!glfwInit()) {
+				std::cout << "Failed to initaialise GLFW" << std::endl;
+				return false;
+			}
+				
+	
+			m_Window = glfwCreateWindow(m_Width, m_Height, m_Title, NULL, NULL);
+
+			if (!m_Window) {
+				glfwTerminate();
+				std::cout << "Failed to create GLFW window!" << std::endl;
+				return false;
+			}
+			glfwMakeContextCurrent(m_Window);
+			glfwSetWindowSizeCallback(m_Window, windowResize);
+			return true;
+		}
+
+		void Window::update() {
+			glfwPollEvents();
+			glfwSwapBuffers(m_Window);
+		}
+
+		void Window::clear() const{
+			glClear(GL_COLOR_BUFFER_BIT);
+		}
+
+		bool Window::closed() const {
+			return glfwWindowShouldClose(m_Window) == 1;
+		}
+		
+		void windowResize(GLFWwindow *window,int width, int height) {
+			glViewport(0, 0, width, height);
+		}
+
+	}
+}
